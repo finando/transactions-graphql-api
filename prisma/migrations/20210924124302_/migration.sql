fcfeec338d6ad830eaff6319104d21949734f809
Alexander Zakharov <alzak@envisage.no>
2021-09-25 16:54:58 +0200

    chore: add missing migration

---
+++ prisma/migrations/20210924124302_/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "Entry" DROP CONSTRAINT "Entry_transactionId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Entry" ADD CONSTRAINT "Entry_transactionId_fkey" FOREIGN KEY ("transactionId") REFERENCES "Transaction"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
